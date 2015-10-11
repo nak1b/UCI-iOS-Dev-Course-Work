06414ab75d6dc3e4330654f073bca440595dceca
Nakib <nbmomin14@gmail.com>
2015-10-10 20:05:46 -0400

    Added hello world in objective c

---
+++ 1 - Foundations of Objective-C App Development/week-1/Hello World/Hello World/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello World
+//
+//  Created by Nakib on 10/10/15.
+//  Copyright © 2015 Nakib. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 1 - Foundations of Objective-C App Development/week-1/Hello World/Hello World/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hello World
+//
+//  Created by Nakib on 10/10/15.
+//  Copyright © 2015 Nakib. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
