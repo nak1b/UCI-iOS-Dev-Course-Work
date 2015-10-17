deb131aca77aea47ed5f370720d7da8c6f0c89a9
Nakib <nbmomin14@gmail.com>
2015-10-17 06:47:39 -0400

    Added cocoapod for the project

---
+++ 1 - Foundations of Objective-C App Development/week-3/currencyConvertor/currencyConvertor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  currencyConvertor
+//
+//  Created by Nakib on 10/17/15.
+//  Copyright © 2015 Nakib. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
