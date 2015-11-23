68255996fdbd4e2d7dbf9afdefd3a92c77880b8b
Nakib <nbmomin14@gmail.com>
2015-11-22 22:08:41 -0500

    Styles textviews and code cleanup

--- 2 - Networking and Security in iOS Applications/Week-1/Social Integration/Social Integration/ViewController.m
+++ 2 - Networking and Security in iOS Applications/Week-1/Social Integration/Social Integration/ViewController.m
@@ -13,6 +13,7 @@
 
 @property (weak, nonatomic) IBOutlet UITextView *twitterTextView;
 @property (weak, nonatomic) IBOutlet UITextView *fbTextView;
+@property (weak, nonatomic) IBOutlet UITextView *activityTextView;
 
 @end
 
@@ -20,7 +21,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self configureTextViewStyle];
+    [self configureTwitterTextViewStyle];
+    [self configureFbTextViewStyle];
+    [self configureActivityTextViewStyle];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -30,7 +33,7 @@
 }
 
 - (void)showAlertMessage:(NSString*) msg{
-    UIAlertController *warningController = [UIAlertController alertControllerWithTitle:msg message:nil preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertController *warningController = [UIAlertController alertControllerWithTitle:@"Social Share" message:msg preferredStyle:UIAlertControllerStyleAlert];
     UIAlertAction *warningAction = [UIAlertAction actionWithTitle:@"Okey" style:UIAlertActionStyleDefault handler:nil];
     [warningController addAction:warningAction];
     [self presentViewController:warningController animated:YES completion:nil];
@@ -64,6 +67,10 @@
 
 //Facebook Sharing
 - (IBAction)fbSharePress:(UIBarButtonItem *)sender {
+    if([self.fbTextView isFirstResponder]){
+        [self.fbTextView resignFirstResponder];
+    }
+    
     if([SLComposeViewController isAvailableForServiceType:SLServiceTypeFacebook]){
         SLComposeViewController *fbController = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
         [fbController setInitialText:self.fbTextView.text];
@@ -75,13 +82,34 @@
 
 //Activity View
 - (IBAction)activityBtnPress:(UIBarButtonItem *)sender {
-    UIActivityViewController *activityController = [[UIActivityViewController alloc] initWithActivityItems:@[self.twitterTextView.text] applicationActivities:nil];
+    if([self.activityTextView isFirstResponder]){
+        [self.activityTextView resignFirstResponder];
+    }
+    
+    UIActivityViewController *activityController = [[UIActivityViewController alloc] initWithActivityItems:@[self.activityTextView.text] applicationActivities:nil];
     [self presentViewController:activityController animated:YES completion:nil];
 }
 
 
-- (void)configureTextViewStyle{
+//Alert Controller
+- (IBAction)showAlertViewController:(UIBarButtonItem *)sender {
+    [self showAlertMessage:@"This do nothing"];
+}
+
+
+//Style for Twitter text views
+- (void)configureTwitterTextViewStyle{
     self.twitterTextView.layer.backgroundColor = [UIColor colorWithRed:252/255.0 green:216/255.0 blue:199/255.0 alpha:1].CGColor;
+}
+
+//Style for facebook text views
+- (void)configureFbTextViewStyle{
+    self.fbTextView.layer.backgroundColor = [UIColor colorWithRed:52/255.0 green:152/255.0 blue:219/255.0 alpha:1.0].CGColor;
+}
+
+//Style fot alert text view
+- (void)configureActivityTextViewStyle{
+     self.activityTextView.layer.backgroundColor = [UIColor colorWithRed:189/255.0 green:195/255.0 blue:199/255.0 alpha:1.0].CGColor;
 }
 
 @end
