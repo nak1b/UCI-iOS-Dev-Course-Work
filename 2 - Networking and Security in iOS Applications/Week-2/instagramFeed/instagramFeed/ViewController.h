af419f67f23743458f25ad2918b160ca2670d8fe
Nakib <nbmomin14@gmail.com>
2015-11-23 19:39:39 -0500

    Crated project for week2 application

---
+++ 2 - Networking and Security in iOS Applications/Week-2/instagramFeed/instagramFeed/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  instagramFeed
+//
+//  Created by Nakib on 11/23/15.
+//  Copyright © 2015 Nakib. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 2 - Networking and Security in iOS Applications/Week-2/instagramFeed/instagramFeed/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  instagramFeed
+//
+//  Created by Nakib on 11/23/15.
+//  Copyright © 2015 Nakib. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
