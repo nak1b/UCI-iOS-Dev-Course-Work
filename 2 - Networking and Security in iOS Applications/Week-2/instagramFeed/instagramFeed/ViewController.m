97c852b50b2410cf3041be650a9e847241addaee
Nakib <nbmomin14@gmail.com>
2015-11-26 21:16:28 -0500

    Requesting data from Instagram and  checking for errors

--- 2 - Networking and Security in iOS Applications/Week-2/instagramFeed/instagramFeed/ViewController.m
+++ 2 - Networking and Security in iOS Applications/Week-2/instagramFeed/instagramFeed/ViewController.m
@@ -22,7 +22,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.logoutBtn.enabled = false;
+    self.refreshBtn.enabled = false;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -32,12 +33,64 @@
 
 - (IBAction)loginBtnPressed:(UIButton *)sender {
     [[NXOAuth2AccountStore sharedStore] requestAccessToAccountWithType:@"Instagram"];
+    self.logoutBtn.enabled = true;
+    self.refreshBtn.enabled = true;
+    self.loginBtn.enabled = false;
 }
 
+
 - (IBAction)logoutBtnPressed:(UIButton *)sender {
+    NXOAuth2AccountStore *store = [NXOAuth2AccountStore sharedStore];
+   
+    //getting all logged in instagram acount
+    NSArray *instagramAccounts = [store accountsWithAccountType:@"Instagram"];
+    
+    //removing any logged in instragm account
+    for(id account in instagramAccounts){
+        [store removeAccount:account];
+    }
+    self.logoutBtn.enabled = false;
+    self.refreshBtn.enabled = false;
+    self.loginBtn.enabled = true;
 }
 
 - (IBAction)refreshBtnPressed:(UIButton *)sender {
+    NSArray *instagramAccounts = [[NXOAuth2AccountStore sharedStore] accountsWithAccountType:@"Instagram"];
+    if([instagramAccounts count]==0){
+        NSLog(@"No account logged in ");
+        return;
+    }
+    
+    NXOAuth2Account *accout = instagramAccounts[0];
+    NSString *token = accout.accessToken.accessToken;
+    
+    NSString *urlString = [@"https://api.instagram.com/v1/users/self/media/recent/?access_token=" stringByAppendingString:token];
+   
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSURLSession *session = [NSURLSession sharedSession];
+    [[session dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        //check for network error
+        if(error){
+            NSLog(@"Can not finish the request %@", error);
+            return;
+        }
+        
+        //check http error
+        NSHTTPURLResponse *httpResp = (NSHTTPURLResponse *) response;
+        if (httpResp.statusCode <300 || httpResp.statusCode >=300) {
+             NSLog(@"Error: Got status code %ld", (long) httpResp.statusCode);
+            return;
+        }
+        
+        //check json parser error
+        NSError *parseErr;
+        id pkg = [NSJSONSerialization JSONObjectWithData:data options:0 error:&parseErr];
+        if(!pkg){
+            NSLog(@"Couldnt parse json %@", parseErr);
+            return;
+        }
+
+    }] resume];
 }
 
 @end
