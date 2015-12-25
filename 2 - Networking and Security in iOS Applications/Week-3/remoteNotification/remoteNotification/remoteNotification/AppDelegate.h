ef368f16e1bf0bc9f162169ddd6ae4d8e606f448
Nakib <nbmomin14@gmail.com>
2015-12-24 18:44:49 -0600

    Created base project for remote notification

---
+++ 2 - Networking and Security in iOS Applications/Week-3/remoteNotification/remoteNotification/remoteNotification/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  remoteNotification
+//
+//  Created by Nakib on 12/24/15.
+//  Copyright © 2015 Nakib. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2 - Networking and Security in iOS Applications/Week-3/remoteNotification/remoteNotification/remoteNotification/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  remoteNotification
+//
+//  Created by Nakib on 12/24/15.
+//  Copyright © 2015 Nakib. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
