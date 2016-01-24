c3967f2274f19df33c4d1b9e7799bf07746ca244
Nakib <nbmomin14@gmail.com>
2016-01-24 18:30:21 -0500

    Initialized the project

---
+++ 2 - Networking and Security in iOS Applications/Week-4/assignment4/contactForm/contactForm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  contactForm
+//
+//  Created by Nakib on 1/24/16.
+//  Copyright © 2016 Nakib. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 2 - Networking and Security in iOS Applications/Week-4/assignment4/contactForm/contactForm/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  contactForm
+//
+//  Created by Nakib on 1/24/16.
+//  Copyright © 2016 Nakib. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
