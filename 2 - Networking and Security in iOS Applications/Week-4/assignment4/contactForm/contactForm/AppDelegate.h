bade259bef4b97e8b2a65f89fc8d220e1e972670
Nakib <nbmomin14@gmail.com>
2016-01-24 23:44:55 -0500

    Adding contact and updating list through UI

--- 2 - Networking and Security in iOS Applications/Week-4/assignment4/contactForm/contactForm/AppDelegate.h
+++ 2 - Networking and Security in iOS Applications/Week-4/assignment4/contactForm/contactForm/AppDelegate.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import <CoreData/CoreData.h>
+#import "ContactMO.h"
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
@@ -20,6 +21,7 @@
 - (void)saveContext;
 - (NSURL *)applicationDocumentsDirectory;
 
+- (ContactMO *) createContactMO;
 
 @end
 
--- 2 - Networking and Security in iOS Applications/Week-4/assignment4/contactForm/contactForm/ViewController.m
+++ 2 - Networking and Security in iOS Applications/Week-4/assignment4/contactForm/contactForm/ViewController.m
@@ -8,10 +8,17 @@
 
 #import "ViewController.h"
 #import "AppDelegate.h"
+#import "ContactMO.h"
 
 @interface ViewController ()
 
 @property (nonatomic) AppDelegate *appDelegate;
+
+@property (weak, nonatomic) IBOutlet UITextField *name;
+@property (weak, nonatomic) IBOutlet UITextField *email;
+@property (weak, nonatomic) IBOutlet UITextField *phone;
+@property (weak, nonatomic) IBOutlet UITextView *contactLogList;
+@property (weak, nonatomic) IBOutlet UILabel *contactCount;
 
 @end
 
@@ -21,7 +28,7 @@
     [super viewDidLoad];
     
     self.appDelegate = [[UIApplication sharedApplication] delegate];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self updateLogList];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -29,4 +36,40 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+- (IBAction)addContact:(UIButton *)sender {
+    ContactMO *contact = [self.appDelegate createContactMO];
+    contact.name = self.name.text;
+    contact.email = self.email.text;
+    contact.phone = self.phone.text;
+    
+    [self.appDelegate saveContext];
+    [self updateLogList];
+}
+
+- (void) updateLogList{
+    NSManagedObjectContext *moc = self.appDelegate.managedObjectContext;
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Contact"];
+    
+    NSError *error = nil;
+    NSArray *results = [moc executeFetchRequest:request error:&error];
+    
+    if(!results){
+        NSLog(@"Error fetching contact object");
+        abort();
+    }
+    
+    NSMutableString *buffer = [NSMutableString stringWithString:@""];
+    
+    for(ContactMO *c in results){
+        [buffer appendFormat:@"\n %@ - %@",c.name, c.email, nil];
+        [buffer appendFormat:@"\n Phone: %@ \n",c.phone,nil];
+    }
+    self.contactLogList.text = buffer;
+    self.contactCount.text = [NSString stringWithFormat:@"%lu", (unsigned long)results.count];
+}
+
+
+- (IBAction)deleteRecords:(UIButton *)sender {
+}
 @end
