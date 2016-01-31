073f9558b9361ff6112ddd3c526f776deabbc94a
Nakib <nbmomin14@gmail.com>
2016-01-30 20:14:21 -0500

    Added layout for potrait mode

---
+++ 3 - Best Practices for iOS User Interface Design/week-1/assignment-1/constraintLayout/constraintLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  constraintLayout
+//
+//  Created by Nakib on 1/30/16.
+//  Copyright © 2016 Nakib. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
