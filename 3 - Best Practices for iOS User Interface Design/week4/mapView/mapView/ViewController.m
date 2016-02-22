a5e854039e7b6f6985a1b75d9c63df9829c20753
Nakib <nbmomin14@gmail.com>
2016-02-22 16:20:56 -0500

    Adding current location to map

--- 3 - Best Practices for iOS User Interface Design/week4/mapView/mapView/ViewController.m
+++ 3 - Best Practices for iOS User Interface Design/week4/mapView/mapView/ViewController.m
@@ -9,13 +9,17 @@
 #import "MapKit/MapKit.h"
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <CLLocationManagerDelegate>
 
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 
 @property (strong, nonatomic) MKPointAnnotation* torontoAnnotation;
 @property (strong, nonatomic) MKPointAnnotation* vancouverAnnotation;
 @property (strong, nonatomic) MKPointAnnotation* houstonAnnotation;
+@property (strong, nonatomic) MKPointAnnotation* currentAnnotation;
+
+@property (weak, nonatomic) IBOutlet UISwitch *switchBtn;
+@property (strong, nonatomic) CLLocationManager *locationManager;
 
 @end
 
@@ -25,6 +29,9 @@
     [super viewDidLoad];
     [self addAnnotation];
     
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    [self.locationManager requestWhenInUseAuthorization];
 }
 
 - (IBAction)torontoClicked:(id)sender {
@@ -37,6 +44,21 @@
 
 - (IBAction)houstonClicked:(id)sender {
      [self centerMap:self.houstonAnnotation];
+}
+
+- (IBAction)switchChanged:(UISwitch *)sender {
+    if(self.switchBtn.isOn){
+        self.mapView.showsUserLocation = YES;
+        [self.locationManager startUpdatingLocation];
+    }else{
+        self.mapView.showsUserLocation = NO;
+        [self.locationManager startUpdatingLocation];
+    }
+}
+
+- (void) locationManager:(CLLocationManager *)manager didUpdateLocations:(nonnull NSArray<CLLocation *> *)locations{
+    self.currentAnnotation.coordinate = locations.lastObject.coordinate;
+    [self centerMap:self.currentAnnotation];
 }
 
 - (void) centerMap: (MKPointAnnotation *) centerPoint {
@@ -56,6 +78,10 @@
     self.houstonAnnotation = [[MKPointAnnotation alloc] init];
     self.houstonAnnotation.coordinate = CLLocationCoordinate2DMake(29.8174782, -95.6814916);
     self.houstonAnnotation.title = @"Houston";
+    
+    self.currentAnnotation = [[MKPointAnnotation alloc] init];
+    self.currentAnnotation.coordinate = CLLocationCoordinate2DMake(0.0, 0.0);
+    self.currentAnnotation.title = @"My Location";
     
     
     [self.mapView addAnnotations: @[self.torontoAnnotation, self.vancouverAnnotation, self.houstonAnnotation]];
