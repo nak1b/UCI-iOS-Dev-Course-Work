a04e47fd2386569c30e1f78f30eda6c9d1407a7b
Nakib <nbmomin14@gmail.com>
2016-03-26 19:48:14 -0400

    Fetching result from db and showing on tableView

--- 3 - Best Practices for iOS User Interface Design/week4/tableview-coredata/tableview-coredata/ViewController.m
+++ 3 - Best Practices for iOS User Interface Design/week4/tableview-coredata/tableview-coredata/ViewController.m
@@ -8,10 +8,13 @@
 
 #import "ViewController.h"
 #import "ToDoEntity.h"
+#import "TableViewCell.h"
 
-@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate, NSFetchedResultsControllerDelegate>
 
  @property (weak, nonatomic) IBOutlet UITextField *todoField;
+
+@property (strong, nonatomic) NSFetchedResultsController *resultsController;
 
 @end
 
@@ -19,7 +22,26 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    
+    fetchRequest.entity = [NSEntityDescription entityForName:@"ToDoEntity" inManagedObjectContext:self.context];
+    
+    fetchRequest.predicate = [NSPredicate predicateWithFormat:@"TRUEPREDICATE"];
+    
+    fetchRequest.sortDescriptors = @[[[NSSortDescriptor alloc] initWithKey:@"title" ascending:YES]];
+    
+    self.resultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest managedObjectContext:self.context sectionNameKeyPath:nil cacheName:nil];
+    
+    self.resultsController.delegate = self;
+    
+    NSError *error;
+    BOOL fetchSucceded = [self.resultsController performFetch:&error];
+    if(!fetchSucceded){
+        @throw [NSException exceptionWithName:NSInternalInconsistencyException reason:@"Couldn't fetch" userInfo:nil];
+    }
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -46,5 +68,24 @@
     }
     
 }
+
+
+#pragma mark - TableViewDelegates
+
+- (NSInteger) numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.resultsController.sections[section].numberOfObjects;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    ToDoEntity *item = self.resultsController.sections[indexPath.section].objects[indexPath.row];
+    TableViewCell *cell = (TableViewCell *)[tableView dequeueReusableCellWithIdentifier:@"TableCell" forIndexPath:indexPath];
+    [cell setTitle:item];
+    return cell;
+}
+
 
 @end
